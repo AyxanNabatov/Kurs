2de08ad34d49ac3d1769d5cbee5e1239b546c284
AyxanNabatov <107032159+AyxanNabatov@users.noreply.github.com>
2022-07-20 13:50:23 +0400

    Add files via upload

---
+++ CourseApp/CourseApp/Domain/Models/Group.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Group : BaseEntity
+    {
+        public string Teacher { get; set; }
+        public string Room { get; set; }
+    }
+}
---
+++ CourseApp/CourseApp/Service/Services/Interfaces/IGroupService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Services.Interfaces
+{
+    public interface IGroupService
+    {
+    }
+}
