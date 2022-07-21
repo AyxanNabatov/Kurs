a49bf03f112ed9828c43451d18549421af068971
AyxanNabatov <107032159+AyxanNabatov@users.noreply.github.com>
2022-07-21 13:17:00 +0400

    Add files via upload

---
+++ kurs2/Kurs/CourseApp/CourseApp/Service/Services/Interfaces/IStudentService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Services.Interfaces
+{
+    public interface IStudentService
+    {
+        StudentCreate(int groupId, Student Student);
+    }
+}
